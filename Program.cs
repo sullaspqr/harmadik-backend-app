2aaca5eef73eabfb553fd69bb657d9d9dde9dba1
HaQT00R <40822691+sullaspqr@users.noreply.github.com>
2025-11-25 21:38:02 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -21,7 +21,7 @@ var app = builder.Build();
 app.UseCors("AllowAll");
 app.UseSwagger();
 app.UseSwaggerUI();
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
 app.Run();
